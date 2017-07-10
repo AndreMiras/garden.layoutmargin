5fd8fe606a8da1e931a77f40ca62276f135176aa
Enteleform <enteleform@gmail.com>
2017-07-10 06:37:33 -0400

    Add files via upload

---
+++ layout_mixins/__init__.py
@@ -0,0 +1,4 @@
+###  SubModules  ###
+from ._add_background import Add_BackgroundColor
+from ._margin_layout  import MarginLayout, AddMargin
+
---
+++ layout_mixins/_add_background.py
@@ -0,0 +1,20 @@
+###  PyPi  ###
+from kivy.properties import ObjectProperty
+from kivy.graphics   import Color, Rectangle
+
+
+class Add_BackgroundColor:
+	background_color = ObjectProperty()
+	def __init__(self, **kwargs):
+		self.bind(background_color=set_CanvasColor)
+
+
+def set_CanvasColor(layout, color):
+	def update_Rect(layout, size):
+		layout.rect.pos  = layout.pos
+		layout.rect.size = layout.size
+	with layout.canvas.before:
+		Color(*color)
+		layout.rect = Rectangle(size=layout.size, pos=layout.pos)
+	layout.bind(size=update_Rect, pos=update_Rect)
+
