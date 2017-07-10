8f050f8a6d4cac6e5373596628b06dc0abd6cff3
Enteleform <enteleform@gmail.com>
2017-07-10 08:20:47 -0400

    Update _add_background.py

--- layout_mixins/_add_background.py
+++ layout_mixins/_add_background.py
@@ -4,17 +4,17 @@ from kivy.graphics   import Color, Rectangle
 
 
 class Add_BackgroundColor:
-	background_color = ObjectProperty()
-	def __init__(self, **kwargs):
-		self.bind(background_color=set_CanvasColor)
+  background_color = ObjectProperty()
+  def __init__(self, **kwargs):
+    self.bind(background_color=set_CanvasColor)
 
 
 def set_CanvasColor(layout, color):
-	def update_Rect(layout, size):
-		layout.rect.pos  = layout.pos
-		layout.rect.size = layout.size
-	with layout.canvas.before:
-		Color(*color)
-		layout.rect = Rectangle(size=layout.size, pos=layout.pos)
-	layout.bind(size=update_Rect, pos=update_Rect)
+  def update_Rect(layout, size):
+    layout.rect.pos  = layout.pos
+    layout.rect.size = layout.size
+  with layout.canvas.before:
+    Color(*color)
+    layout.rect = Rectangle(size=layout.size, pos=layout.pos)
+  layout.bind(size=update_Rect, pos=update_Rect)
 
